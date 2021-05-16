89c71873c67d2880ab86a3f1b1f8ba8d637497ae
Xue <Xue@Mac-deMacBook-Pro.local>
2021-05-16 00:14:26 -0700

    fix conflict issues

--- wallet/cmd/main.go
+++ wallet/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"bufio"
 	"crypto/rand"
 	"crypto/rsa"
@@ -15,14 +14,6 @@ import (
 	"strings"
 
 	"github.com/Luismorlan/btc_in_go/commands"
-=======
-	"crypto/rand"
-	"crypto/rsa"
-	"flag"
-	"fmt"
-	"log"
-
->>>>>>> master
 	"github.com/Luismorlan/btc_in_go/utils"
 	"github.com/Luismorlan/btc_in_go/wallet"
 )
@@ -40,13 +31,8 @@ var (
 
 func init() {
 	fPathFlag = flag.String("fPath", "", "RSA file path for your private key")
-<<<<<<< HEAD
 	ipAddrFlag = flag.String("ip", DEFAULT_FN_IP, "ip address of full node")
 	portFlag = flag.String("port", DEFAULT_FN_PORT, "Port number for connection")
-=======
-	ipAddrFlag = flag.String("ip", "127.0.0.1", "ip address of full node")
-	portFlag = flag.String("port", "8280", "Port number for connection")
->>>>>>> master
 }
 
 func main() {
@@ -60,7 +46,6 @@ func main() {
 		log.Println("[ERROR]Failed to get your wallet. Please check the input.")
 		return
 	}
-<<<<<<< HEAD
 	log.Println("Wallet logged in")
 	cmd := make(chan commands.ClientCommand)
 	go ParseCommand(cmd)
@@ -98,31 +83,15 @@ func HandleCommand(cmd chan commands.ClientCommand, wallet *wallet.Wallet) {
 			fmt.Print("> ")
 		}
 	}
-=======
-	log.Println(wallet)
-
-	// reader := bufio.NewReader(os.Stdin)
-
->>>>>>> master
 }
 
 func parseToWallet(fPath string, ipAddr string, port string) (*wallet.Wallet, error) {
 	var wallet wallet.Wallet
 	var err error
-<<<<<<< HEAD
 	wallet.FullNodeIp = ipAddr
 	wallet.FullNodePort = port
 	wallet.Keys, err = parseKeyFile(fPath)
 	return &wallet, err
-=======
-	wallet.Keys, err = parseKeyFile(fPath)
-	if err != nil {
-		return nil, err
-	}
-	wallet.FullNodeIp = ipAddr
-	wallet.FullNodePort = port
-	return &wallet, nil
->>>>>>> master
 }
 
 func parseKeyFile(fPath string) (*rsa.PrivateKey, error) {
@@ -131,15 +100,11 @@ func parseKeyFile(fPath string) (*rsa.PrivateKey, error) {
 	if fPath == "" || fPath == "\n" {
 		log.Println("No key file detected. Will generate a new key for you")
 		userKey, err = rsa.GenerateKey(rand.Reader, 2048)
-<<<<<<< HEAD
 		// TODO : add new key in user path
-=======
->>>>>>> master
 		if err != nil {
 			log.Fatal("Got error when generating new key", err)
 			return nil, err
 		}
-<<<<<<< HEAD
 		return userKey, nil
 	}
 	// read from rsa file
@@ -149,7 +114,6 @@ func parseKeyFile(fPath string) (*rsa.PrivateKey, error) {
 		return nil, err
 	}
 
-=======
 	} else {
 		// read from rsa file
 		userKey, err = readKeyFromFPath(fPath)
@@ -158,12 +122,6 @@ func parseKeyFile(fPath string) (*rsa.PrivateKey, error) {
 			return nil, err
 		}
 	}
->>>>>>> master
-	key := utils.BytesToHex(utils.PublicKeyToBytes(&userKey.PublicKey))
-
-	fmt.Println(key)
-	return userKey, err
-<<<<<<< HEAD
 }
 
 func readKeyFromFPath(fPath string) (*rsa.PrivateKey, error) {
@@ -174,11 +132,4 @@ func readKeyFromFPath(fPath string) (*rsa.PrivateKey, error) {
 	block, _ := pem.Decode([]byte(fileContent))
 	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
 	return key, nil
-=======
-
-}
-
-func readKeyFromFPath(fPath string) (*rsa.PrivateKey, error) {
-	return nil, nil
->>>>>>> master
 }
